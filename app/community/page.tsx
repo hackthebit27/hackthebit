7f56cf27fed580d80f581125e1eccf270ed502fc
Vivek Yadav <vy30289@gmail.com>
2025-08-23 16:51:03 +0530

    Initial commit

---
+++ app/community/page.tsx
@@ -0,0 +1,8 @@
+export default function CommunityPage() {
+  return (
+    <div className="py-20 text-center">
+      <h1 className="text-4xl font-bold">Community</h1>
+      <p className="mt-4 text-lg text-gray-600">Will display Community here.</p>
+    </div>
+  );
+}
