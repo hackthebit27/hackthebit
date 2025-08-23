562c7f87728c4af9890c520a3ea68915a6b39287
Vivek Yadav <vy30289@gmail.com>
2025-08-23 16:46:38 +0530

    Initial commit

---
+++ app/mentors/page.tsx
@@ -0,0 +1,8 @@
+export default function MentorsPage() {
+  return (
+    <div className="py-20 text-center">
+      <h1 className="text-4xl font-bold">Mentors</h1>
+      <p className="mt-4 text-lg text-gray-600">Will display Mentors here.</p>
+    </div>
+  );
+}
